5b7b1c7f6586560a5f210d01f01aca5c4724804b
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-04-04 10:45:31 +0530

    feat: added logger, env.ts and app

---
+++ services/auth-service/src/index.ts
@@ -0,0 +1,21 @@
+import { createServer } from "http";
+import { createApp } from "./app";
+import { env } from "./config/env";
+import { logger } from "./utils/logger";
+
+const main = async () => {
+  try {
+    const app = createApp();
+    const server = createServer(app);
+    const port = env.AUTH_SERVICE_PORT;
+
+    server.listen(port, () => {
+      logger.info({ port }, "Auth service is running.");
+    });
+  } catch (error) {
+    logger.error({ error }, "Failed to start auth service.");
+    process.exit(1);
+  }
+};
+
+void main();
