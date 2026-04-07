fabf432c7aa4b6599f486e3a2bec69719d562ecb
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-04-07 12:19:19 +0530

    fix: .gitignore

--- packages/common/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './env.js';
-export * from './logger.js';
-export * from './errors/http-error.js';
-export * from './http/async-handler.js';
-export { z } from 'zod';
-export type { Logger } from 'pino';
-//# sourceMappingURL=index.d.ts.map
--- packages/common/dist/index.js
+++
@@ -1,6 +0,0 @@
-export * from './env.js';
-export * from './logger.js';
-export * from './errors/http-error.js';
-export * from './http/async-handler.js';
-export { z } from 'zod';
-//# sourceMappingURL=index.js.map
