0eee166971ce9462e0162771a3ef59230e30ceec
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-04-08 15:15:52 +0530

    feat(gateway): gateway service setup

---
+++ services/gateway-service/src/app.ts
@@ -0,0 +1,28 @@
+import express, { type Application } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import { errorHandler } from './middleware/error-handler.js';
+// import { registerRoutes } from './routes/index.js';
+
+export const createApp = (): Application => {
+  const app = express();
+
+  app.use(helmet());
+  app.use(
+    cors({
+      origin: '*',
+      credentials: true,
+    }),
+  );
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  //registerRoutes(app);
+
+  app.use((_req, res) => {
+    res.status(404).json({ message: 'Not Found' });
+  });
+
+  app.use(errorHandler);
+  return app;
+};
---
+++ services/gateway-service/src/config/env.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import { createEnv, z } from '@micro-chat/common';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+  GATEWAY_PORT: z.coerce.number().int().min(0).max(65_535).default(4000),
+  AUTH_SERVICE_URL: z.string().url(),
+});
+
+type EnvType = z.infer<typeof envSchema>;
+
+export const env: EnvType = createEnv(envSchema, {
+  serviceName: 'auth-service',
+});
+export type Env = typeof env;
