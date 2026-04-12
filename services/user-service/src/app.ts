442c01b144e6035a198a763a1a9e578f506c747f
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-04-12 13:10:13 +0530

    feat(user): basic setup for user-service

---
+++ services/user-service/src/app.ts
@@ -0,0 +1,30 @@
+import express, { type Application } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import { errorHandler } from './middleware/error-handler.js';
+import { createInternalAuthMiddleware } from '@micro-chat/common';
+import { env } from './config/env.js';
+
+export const createApp = (): Application => {
+  const app = express();
+
+  app.use(helmet());
+  app.use(
+    cors({
+      origin: '*',
+      credentials: true,
+    }),
+  );
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(createInternalAuthMiddleware(env.INTERNAL_API_TOKEN, { exemptPaths: ['/users/health'] }));
+
+  //   registerRoutes(app);
+
+  app.use((_req, res) => {
+    res.status(404).json({ message: 'Not Found' });
+  });
+
+  app.use(errorHandler);
+  return app;
+};
